fc14df975bbc3c286d00682582f9015aee69b6ef
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2018-01-04 20:03:05 -0600

    working on bio page and mobile first

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import cat from '../images/cat.jpg';
+import me from '../images/me.jpg';
 
 class Bio extends Component {
   render () {
     return (
       <div>
         <h1>Bio</h1>
+        <div className="info1">
+          <img src={cat}/>
+          <img src={me}/>
+          <p>
+            Hailing from the great city of Memphis in Tennessee, Stefanie has interests ranging from good barbeque to listening to live blues and jazz. Her passion for education, the arts, and sciences began in early life during volunteer work, exploring the outdoors through hiking, biking, swimming, and climbing, and classes at school.
+          </p>
+        </div>
+        <div className="info2">
+          <p>
+            Currently, Stefanie lives in a cozy spot of the Dallas area, living with her cat, Daniel, her boyfriend, and a roomate. On weekends, she codes, paints, and finds time to practice digital art. She frequents art contests, craft fairs, curio shops and book stores, and museums around town.
+          </p>
+        </div>
       </div>
     )
   }
--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/MainNav.css';
 
 class MainNav extends Component {
   render() {
     return (
-      <div>
+      <div classname="wrapperDiv">
         <nav>
           <NavLink activeClassName="selected" className="nav-link" exact to="/Home">Home</NavLink>
           <NavLink activeClassName="selected" className="nav-link" exact to="/Portfolio">Portfolio</NavLink>
           <NavLink activeClassName="selected" className="nav-link" exact to="/Bio">Bio</NavLink>
           <NavLink activeClassName="selected" className="nav-link" exact to="/Contact">Contact</NavLink>
         </nav>
+        <div className="wrapperForContent">
           {this.props.children}
+        </div>
       </div>
     );
   }
